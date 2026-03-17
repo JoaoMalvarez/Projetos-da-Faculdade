2c1faeb8ff8f1a976a045048d517f79f8007a9c8
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-03-17 11:45:42 +0000

    Classe pilha

---
+++ Torre_de_Hannoi-ED/Pilha.java
@@ -0,0 +1,59 @@
+public class Pilha <T> {
+    // Atributos:
+    private static final int TAM = 100;
+    private int topoPilha;
+    private T elementos[];
+    // Construtores:
+
+    public Pilha(int tamanho) {
+        this.elementos = (T[]) new Object[tamanho];
+        this.topoPilha = -1; 
+    }
+
+    public Pilha() {
+        this(TAM);
+    }
+    // Métodos:
+
+    public boolean isEmpty() {
+        return this.topoPilha == -1;
+    }
+
+    public boolean isFull() {
+        return this.topoPilha == elemntos.length -1;
+    }
+
+    public void push(T e) throws Exception {
+        if(!this.isFull()) {
+            topoPilha++;
+            this.elementos[topoPilha] = e;
+        }       
+        else {
+            throw new Exception("Overflow: Pilha está cheia"); //talvez mude
+        }
+    }
+
+    public void pop() throws Exception{
+        if(!this.isEmpty) {
+            T temp = this.elementos[topoPilha];
+            topoPilha--;
+            return temp;
+        }
+        else {
+            throw new Exception("Underflow: Pilha já está vazia"); //talvez mude
+        }
+    }
+
+    public T topo() throws Exception {
+        if (!this.isEmpty()) {
+            return this.elementos[topoPilha];
+        }
+        else {
+            throw new Exception("Underflow: A pilha está vazia")
+        }
+    }
+
+    public int sizeElements() {
+        return topoPilha + 1;
+    }
+}
