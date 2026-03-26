2e2ad866e10bf696a40f8e5f2acd8f96a7205863
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-03-26 11:17:53 -0300

    Atualizar o Hannoi.java

--- Torre_de_Hannoi-ED/Hannoi.java
+++ Torre_de_Hannoi-ED/Hannoi.java
@@ -10,7 +10,6 @@ public class Hannoi {
         Torre<Disco> torre2 = new Torre<>(Comparator.comparingInt(Disco::getTamanho));  
         Torre<Disco> torre3 = new Torre<>(Comparator.comparingInt(Disco::getTamanho));
         Print<Disco> print = new dequePrint<>();
-        Reserva<Disco> reserva = new dequePrint<>();
         //adicionar os discos na torre 1
         for (int i = numDiscos; i >= 1; i--) {
             torre1.push (new Disco("*".repeat(i), i), numDiscos); // a forma vai sair *, **, ***...
@@ -22,18 +21,18 @@ public class Hannoi {
         for (int i = numDiscos; i >= 1; i--) {
             //coloca os valores nas torres do deque
             print.enqueueLeft(torre3.pop());
-            reserva.enqueueRight(print.getLeft());
             print.enqueueLeft(torre2.pop());
-            reserva.enqueueRight(print.getLeft());
             print.enqueueLeft(torre1.pop());
-            reserva.enqueueRight(print.getLeft());
         }
         for (int i = numDiscos; i > 0; i--) {
             //imprime os valores nas torres
+            torre1.push(print.getLeft());
             System.out.print(print.dequeueLeft());
             System.out.print("  " .repeat(i));
+            torre2.push(print.getLeft());
             System.out.print(print.dequeueLeft());
             System.out.print("  " .repeat(i));
+            torre3.push(print.getLeft());
             System.out.println(print.dequeueLeft());
 
             if (i == 1) {
@@ -45,22 +44,7 @@ public class Hannoi {
                 System.out.print("  ".repeat(numDiscos - 7));
             }
         }
-        for (int i = numDiscos; i > 0; i--) {
-            //reepopulando as torres
-            torre1.push(reserva.dequeueRight());
-            torre2.push(reserva.dequeueRight());
-            torre3.push(reserva.dequeueRight());
-        }
-        
-        System.out.println("Torres:");
-        Pilha<Disco> fila = new Pilha<>();
-        while (!torre1.isEmpty()) {
-            fila.push(torre1.pop());
-        }   
-
-        while (!fila.isEmpty()) {
-            System.out.println(fila.pop());
-        }
+       
     scanner.close();
     }
 }
